4016b321129f285ff3ae9d5eea3c3ec0b4d633c9
Siddharth Bagal <48718441+imsid007@users.noreply.github.com>
2025-02-10 13:09:53 +0530

    icon category by id (#442)

--- src/components/SvgSpriteLoader/index.tsx
+++ src/components/SvgSpriteLoader/index.tsx
@@ -2,6 +2,7 @@ import React, { memo, MouseEventHandler } from 'react';
 import { ICON_SPRITE_TYPES } from 'constants/icons';
 import dynamic from 'next/dynamic';
 import { defaultFn } from 'types/commonTypes';
+import { SPRITE_CATEGORY_BY_ID } from 'components/SvgSpriteLoader/svgSpriteLoader.constants';
 import 'external-svg-loader';
 
 export interface SvgSpriteLoaderProps {
@@ -26,7 +27,7 @@ const SvgSpriteLoader: React.FC<SvgSpriteLoaderProps> = ({
   viewBox = '0 0 24 24',
   fillColor = 'none',
   color,
-  iconCategory = ICON_SPRITE_TYPES.ALERTS_AND_FEEDBACK,
+  iconCategory,
   id = 'alert-circle',
   version = 0,
   domain = 'https://assets.zamp.finance',
@@ -34,20 +35,24 @@ const SvgSpriteLoader: React.FC<SvgSpriteLoaderProps> = ({
   className = '',
   onClick = defaultFn,
   customSpriteUrl,
-}) => (
-  <div onClick={onClick} className={className} data-testid={`svg-sprite-loader-${id}`}>
-    <svg
-      id={id}
-      viewBox={viewBox}
-      width={width}
-      height={height}
-      fill={fillColor}
-      color={color}
-      data-src={customSpriteUrl ?? `${domain}/sprites/v${version}/${iconCategory}.svg#${id}`}
-      data-cache={dataCache}
-    ></svg>
-  </div>
-);
+}) => {
+  const category = iconCategory ?? SPRITE_CATEGORY_BY_ID[id];
+
+  return (
+    <div onClick={onClick} className={className} data-testid={`svg-sprite-loader-${id}`}>
+      <svg
+        id={id}
+        viewBox={viewBox}
+        width={width}
+        height={height}
+        fill={fillColor}
+        color={color}
+        data-src={customSpriteUrl ?? `${domain}/sprites/v${version}/${category}.svg#${id}`}
+        data-cache={dataCache}
+      ></svg>
+    </div>
+  );
+}
 
 // Export with memoization
 const MemoizedSvgSpriteLoader = memo(SvgSpriteLoader);
