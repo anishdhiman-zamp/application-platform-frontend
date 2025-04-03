bffe34d0a7e639758a6214887a972dd560ceb1ed
Siddharth Bagal <48718441+imsid007@users.noreply.github.com>
2025-04-03 12:32:54 +0530

    remove icon

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -27,7 +27,6 @@ export const GROUP_EXPAND_ICON = IMAGE_PREFIX + '/icons/group-expand.svg';
 export const GROUP_COLLAPSE_ICON = IMAGE_PREFIX + '/icons/group-collapse.svg';
 export const CONNECT_ACCOUNT = IMAGE_PREFIX + '/images/connect-account.svg';
 export const DATASET_TABLE = IMAGE_PREFIX + '/icons/dataset-table.svg';
-export const CHECK_GREEN = IMAGE_PREFIX + '/icons/check-green.svg';
 
 // Bank Icons
 export const ADCB_SVG = IMAGE_PREFIX + '/icons/bank-icons/adcb.svg';
