7a5f796da0ce40f546dc480947ddf51c3959e896
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2025-04-09 11:20:42 +0530

    chore/ moved file

--- apps/application-dashboard/src/modules/data/components/importDataset/ImportFile.tsx
+++ apps/application-dashboard/src/modules/data/components/importDataset/ImportFile.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from 'react';
 import FileUploader from 'modules/data/components/importDataset/FileUploader';
-import FileUploaderWrapper from 'modules/data/components/importDataset/FileUploaderWrapper';
 import { FILE_SIZE } from 'modules/data/components/importDataset/importData.constants';
 import { FILE_MIME, ImportFilePropsType } from 'modules/data/components/importDataset/importData.types';
 import { UploadFileResponseType } from 'types/api/dataset.types';
 import { MapAny } from 'types/commonTypes';
 import * as XLSX from 'xlsx';
+import FileUploaderWrapper from '@/components/file-upload/FileUploaderWrapper';
 
 const ImportFile: FC<ImportFilePropsType> = ({
   acceptedFormats,
