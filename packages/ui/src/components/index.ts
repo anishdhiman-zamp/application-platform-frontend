327f5bec438ca6dda88c87239f4c26bb920938a2
Aditya Jain <aditya.jain@zamp.ai>
2025-04-10 22:09:46 +0530

    revert the changes

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,3 +1,5 @@
-export * from './ui/button';
-export * from './ui/input';
-export * from './ui/tabs';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+import { Tabs } from './ui/tabs';
+
+export { Button, Input, Tabs };
