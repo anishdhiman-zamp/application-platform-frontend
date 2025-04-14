78c5bbc291c10b1c80883c1a827ef5dd45b34b39
Aditya Jain <aditya.jain@zamp.ai>
2025-04-14 11:13:26 +0530

    squash the commits

--- apps/application-dashboard/src/instrumentation-client.ts
+++ apps/application-dashboard/src/instrumentation-client.ts
@@ -1,10 +1,7 @@
-// This file configures the initialization of Sentry on the client.
-// The config you add here will be used whenever a users loads a page in their browser.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from '@sentry/nextjs';
 import { browserTracingIntegration, replayIntegration } from '@sentry/nextjs';
 import { SENTRY_DSN } from 'constants/common.constants';
+
 export const ENVIRONMENT = process.env.NEXT_PUBLIC_ENVIRONMENT;
 
 if (ENVIRONMENT === 'production') {
