59f63081b398a7fd0de9b777e2e9a509a60c3f7a
Tushar Agarwal <tushar@zamp.ai>
2025-04-30 18:35:13 +0530

    datasetId destructure error from usePrams (#165)

--- apps/application-dashboard/src/pages/datasets/[datasetId].tsx
+++ apps/application-dashboard/src/pages/datasets/[datasetId].tsx
@@ -4,9 +4,9 @@ import { useParams } from 'next/navigation';
 import DashboardLayout from 'components/layouts/dashboard-layout';
 
 const Dataset = () => {
-  const { datasetId } = useParams();
+  const params = useParams();
 
-  return <DatasetById id={datasetId as string} />;
+  return <DatasetById id={params?.datasetId as string} />;
 };
 
 Dataset.getLayout = function getLayout(page: ReactElement) {
