2ca1e4911cade7ad43d392016c6d67fcef6a2cf5
chiragmittal-zamp <chirag@zamp.ai>
2025-05-01 13:25:19 +0530

    don't wrap attribute

--- packages/ui/src/components/ui/attribute.tsx
+++ packages/ui/src/components/ui/attribute.tsx
@@ -11,8 +11,8 @@ export const Attribute = ({ label, displayValue, ...props }: AttributeProps) => 
       {...props}
       className='inline-flex items-center gap-1.5 rounded-md border border-GRAY_400 px-2 py-1 hover:bg-BG_GRAY_2 active:bg-BG_GRAY_2'
     >
-      <span className='text-secondary-400 f-12-400'>{label}</span>
-      <span className='text-black f-12-500'>{displayValue}</span>
+      <span className='text-secondary-400 f-12-400 whitespace-nowrap'>{label}</span>
+      <span className='text-black f-12-500 whitespace-nowrap'>{displayValue}</span>
     </button>
   );
 };
