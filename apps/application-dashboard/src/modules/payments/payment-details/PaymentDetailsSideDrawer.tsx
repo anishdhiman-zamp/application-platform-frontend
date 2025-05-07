9c508a8e6d2899e8ff989b8e3b3bccd92c0b0c49
SidAtZamp <siddharth@zamp.ai>
2025-05-07 08:23:05 +0530

    approval details dropdown (#239)
    * page icon
    
    * colors update
    
    * review icon fix
    
    * approval details dropdown
    
    * hide button
    
    * hide button

--- apps/application-dashboard/src/modules/payments/payment-details/PaymentDetailsSideDrawer.tsx
+++ apps/application-dashboard/src/modules/payments/payment-details/PaymentDetailsSideDrawer.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useMemo, useState } from 'react';
 import { Sheet, SheetContent } from '@zamp-platform/ui';
 import ApproveActionCard from 'modules/payments/payment-details/components/ApproveActionCard';
 import PaymentApprovals from 'modules/payments/payment-details/PaymentApprovals';
@@ -7,6 +7,8 @@ import { PAYMENT_DETAILS_TABS } from 'modules/payments/payments.constant';
 import { PAYMENT_TABS } from 'modules/payments/payments.types';
 import { defaultFnType } from 'types/commonTypes';
 import { useGetPaymentApprovalsInfoQuery } from '@/apis/payments';
+import CommonWrapper from '@/components/commonWrapper';
+import { SkeletonTypes } from '@/components/commonWrapper/commonWrapper.types';
 import { cn } from '@/utils/common';
 
 type PaymentDetailsSideDrawerProps = {
@@ -20,7 +22,7 @@ const PaymentDetailsSideDrawer: FC<PaymentDetailsSideDrawerProps> = ({ onClose, 
     if (value) setCurrentTab(value);
   };
 
-  const { data: paymentApprovalsInfo } = useGetPaymentApprovalsInfoQuery(paymentDetailsId);
+  const { data: paymentApprovalsInfo, isFetching } = useGetPaymentApprovalsInfoQuery(paymentDetailsId);
 
   const renderTabContent = () => {
     switch (currentTab) {
@@ -31,28 +33,48 @@ const PaymentDetailsSideDrawer: FC<PaymentDetailsSideDrawerProps> = ({ onClose, 
     }
   };
 
+  const tabsList = useMemo(() => {
+    return PAYMENT_DETAILS_TABS.filter(
+      (tab) =>
+        (tab.value === PAYMENT_TABS.APPROVALS &&
+          !!paymentApprovalsInfo?.policy_evaluation_data?.approval_flow?.steps?.length) ||
+        tab.value !== PAYMENT_TABS.APPROVALS,
+    );
+  }, [paymentApprovalsInfo]);
+
   return (
     <Sheet open={!!paymentDetailsId} onOpenChange={onClose}>
       <SheetContent size='medium' tabIndex={-1} className='p-0 h-screen overflow-hidden'>
         <div className='h-full flex flex-col' tabIndex={-1}>
-          <div className='px-4.5 py-6 flex items-center gap-3 border-b border-GRAY_400'>
-            {PAYMENT_DETAILS_TABS.map((tab) => (
-              <div
-                key={tab.value}
-                className={cn(
-                  'px-1.5 py-1 rounded-md flex gap-1.5 items-center',
-                  currentTab === tab.value ? 'f-12-500 bg-GRAY_100' : 'f-12-450',
-                  'cursor-pointer',
-                )}
-                onClick={() => handleTabSelect(tab.value)}
-              >
-                <div className='f-12-500'>{tab.label}</div>
-                {tab.value === 'approvals' && paymentApprovalsInfo?.approval_id && (
-                  <div className='w-1.5 h-1.5 rounded-full bg-ORANGE_700' />
-                )}
+          <CommonWrapper
+            isLoading={isFetching}
+            loader={
+              <div className='px-4.5 py-6 flex items-center gap-3 border-b border-GRAY_400 animate-pulse'>
+                <div className='w-30 h-6 rounded-md bg-GRAY_100 ' />
+                <div className='w-30 h-6 rounded-md bg-GRAY_100' />
               </div>
-            ))}
-          </div>
+            }
+            skeletonType={SkeletonTypes.CUSTOM}
+          >
+            <div className='px-4.5 py-6 flex items-center gap-3 border-b border-GRAY_400'>
+              {tabsList?.map((tab) => (
+                <div
+                  key={tab.value}
+                  className={cn(
+                    'px-1.5 py-1 rounded-md flex gap-1.5 items-center',
+                    currentTab === tab.value ? 'f-12-500 bg-GRAY_100' : 'f-12-450',
+                    'cursor-pointer',
+                  )}
+                  onClick={() => handleTabSelect(tab.value)}
+                >
+                  <div className='f-12-500'>{tab.label}</div>
+                  {tab.value === 'approvals' && paymentApprovalsInfo?.approval_id && (
+                    <div className='w-1.5 h-1.5 rounded-full bg-ORANGE_700' />
+                  )}
+                </div>
+              ))}
+            </div>
+          </CommonWrapper>
           {paymentApprovalsInfo?.approval_id && <ApproveActionCard approvalId={paymentApprovalsInfo?.approval_id} />}
           {renderTabContent()}
         </div>
--- apps/application-dashboard/src/modules/payments/payment-details/components/ApprovalDetailsBadge.tsx
+++ apps/application-dashboard/src/modules/payments/payment-details/components/ApprovalDetailsBadge.tsx
@@ -1,9 +1,10 @@
 import { FC, useMemo } from 'react';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@zamp-platform/ui';
 import { APPROVAL_TYPE } from 'modules/payments/payments.constant';
 import SvgSpriteLoader from '@/components/SvgSpriteLoader';
 import { COLORS } from '@/constants/colors';
 import type { AudiencesByOrganisationIdResponse, GetTeamsByOrganizationIdResponseType } from '@/types/api/people.types';
-import type { ApproverDetailsType } from '@/types/api/policies.types';
+import { ApprovalStatus, type ApproverDetailsType } from '@/types/api/policies.types';
 import { cn } from '@/utils/common';
 
 type ApprovalDetailsBadgeProps = {
@@ -11,60 +12,107 @@ type ApprovalDetailsBadgeProps = {
   teamsData: GetTeamsByOrganizationIdResponseType[];
   orgMembers: AudiencesByOrganisationIdResponse[];
   orgName: string;
+  isApproved: boolean;
 };
 
 const STATUS_ICON_MAP = {
   APPROVED: <SvgSpriteLoader id='check' color={COLORS.GRAY_1000} size={12} />,
   PENDING: <SvgSpriteLoader id='clock' color={COLORS.GRAY_700} size={12} />,
-  REJECTED: <SvgSpriteLoader id='close' color={COLORS.ORANGE_200} size={12} />,
+  REJECTED: <SvgSpriteLoader id='x-close' color={COLORS.ORANGE_200} size={12} />,
 };
 
-const ApprovalDetailsBadge: FC<ApprovalDetailsBadgeProps> = ({ approvalDetails, teamsData, orgMembers, orgName }) => {
-  const teamLength = useMemo(() => {
-    return approvalDetails?.approval_status_details?.user_approvals?.length;
-  }, [approvalDetails]);
+const ApprovalDetailsBadge: FC<ApprovalDetailsBadgeProps> = ({
+  approvalDetails: {
+    approval_status_details: { user_approvals, status },
+    id,
+    type,
+  },
+  teamsData,
+  orgMembers,
+  orgName,
+  isApproved,
+}) => {
+  const approvalUsers = useMemo(() => {
+    return user_approvals;
+  }, [user_approvals]);
 
-  const { label, backgroundColor, borderColor } = useMemo(() => {
-    switch (approvalDetails.type) {
+  const getUserStatusIcon = (userId: string) => {
+    const user = approvalUsers?.find((user) => user.id === userId);
+
+    if (user?.status) {
+      return STATUS_ICON_MAP[user.status as keyof typeof STATUS_ICON_MAP];
+    }
+
+    return <SvgSpriteLoader id='clock' color={COLORS.GRAY_500} size={12} />;
+  };
+
+  const { label, backgroundColor, borderColor, teamList } = useMemo(() => {
+    switch (type) {
       case APPROVAL_TYPE.TEAM: {
-        const team = teamsData?.find((team) => team?.team_id === approvalDetails?.id);
+        const team = teamsData?.find((team) => team?.team_id === id);
 
         return {
           label: team?.name,
           backgroundColor: team?.metadata?.color_hex_code,
           borderColor: team?.metadata?.color_hex_code,
+          teamList: team?.team_memberships?.map((team) => team?.user),
         };
       }
       case APPROVAL_TYPE.USER: {
-        const orgMember = orgMembers?.find((member) => member?.resource_audience_id === approvalDetails?.id);
+        const orgMember = orgMembers?.find((member) => member?.resource_audience_id === id);
 
         return {
           label: orgMember?.user?.name,
           backgroundColor: COLORS.WHITE,
           borderColor: COLORS.GRAY_400,
+          teamList: [],
         };
       }
-      case APPROVAL_TYPE.ORGANIZATION:
+      case APPROVAL_TYPE.ORGANIZATION: {
         return {
           label: orgName,
           backgroundColor: COLORS.WHITE,
           borderColor: COLORS.GRAY_400,
+          teamList: orgMembers.map((res) => res.user),
         };
+      }
+      default: {
+        return { label: '', backgroundColor: COLORS.WHITE, borderColor: COLORS.GRAY_400 };
+      }
     }
-
-    return { label: 'user', backgroundColor: COLORS.WHITE, borderColor: COLORS.GRAY_400 };
-  }, [teamsData, approvalDetails]);
+  }, [teamsData, type]);
 
   return (
-    <div
-      className={cn('px-1.5 py-0.5 rounded flex items-center gap-1 border')}
-      style={{ background: backgroundColor, borderColor }}
-    >
-      {STATUS_ICON_MAP[approvalDetails?.approval_status_details?.status as keyof typeof STATUS_ICON_MAP]}
-      <div className='f-12-500 text-GRAY_1000'>
-        {label} <span className='text-GRAY_800'>{!!teamLength && teamLength}</span>
-      </div>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div
+          className={cn('px-1.5 py-0.5 rounded flex items-center gap-1 border', {
+            'cursor-pointer': !!teamList?.length,
+          })}
+          style={{ background: backgroundColor, borderColor }}
+        >
+          {(!isApproved || (isApproved && status === ApprovalStatus.APPROVED)) &&
+            STATUS_ICON_MAP[status as keyof typeof STATUS_ICON_MAP]}
+          <div className='f-12-500 text-GRAY_1000 select-none'>{label}</div>
+        </div>
+      </DropdownMenuTrigger>
+      {!!teamList?.length && (
+        <DropdownMenuContent
+          align='end'
+          className='z-[1001] min-w-[200px]  max-h-[300px] overflow-y-auto'
+          sideOffset={5}
+        >
+          {teamList?.map((user) => (
+            <DropdownMenuItem key={user?.user_id} className='flex items-center justify-between py-1 cursor-default'>
+              <div className='f-12-450 text-GRAY_1000 px-2.5 py-0.5 border border-GRAY_400 rounded'>
+                {user?.name ?? ''}
+              </div>
+              {getUserStatusIcon(user?.user_id)}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      )}
+    </DropdownMenu>
   );
 };
 
