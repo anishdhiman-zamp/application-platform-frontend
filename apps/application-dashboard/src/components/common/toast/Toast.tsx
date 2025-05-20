8d01d5e99541f6b2ccb7eed86d0e045aa351f89d
Tushar Agarwal <tushar@zamp.ai>
2025-05-20 17:02:53 +0530

    updated stories and dashboard function

--- apps/application-dashboard/src/components/common/toast/Toast.tsx
+++ apps/application-dashboard/src/components/common/toast/Toast.tsx
@@ -1,74 +1,4 @@
-// import { FC, useMemo } from 'react';
-// import { SvgSpriteLoader } from '@zamp-platform/ui/assets';
-// import { COLORS } from 'constants/colors';
-// import { ICON_SPRITE_TYPES } from 'constants/icons';
-// import { CustomToastPropsType } from 'components/common/toast/toast.types';
 import { toast as sonnerToast } from '@zamp-platform/ui';
-
-// const CustomToast: FC<CustomToastPropsType> = ({ text = '' }) => {
-//   return <div className='f-14-400 flex w-full items-center gap-6 text-GRAY_700 -ml-[8px] -mt-[2px]'>{text}</div>;
-// };
-
-// const closeToast = () => {
-//   sonnerToast.dismiss();
-// };
-
-// const GetToastIcon = (type: string) => {
-//   return useMemo(() => {
-//     switch (type) {
-//       case 'success':
-//         return (
-//           <SvgSpriteLoader
-//             id='check-circle'
-//             iconCategory={ICON_SPRITE_TYPES.GENERAL}
-//             width={16}
-//             height={16}
-//             color={COLORS.GREEN_PRIMARY}
-//           />
-//         );
-//       case 'error':
-//         return (
-//           <SvgSpriteLoader
-//             id='x-circle'
-//             iconCategory={ICON_SPRITE_TYPES.GENERAL}
-//             width={16}
-//             height={16}
-//             color={COLORS.RED_PRIMARY}
-//           />
-//         );
-//       case 'warning':
-//         return (
-//           <SvgSpriteLoader
-//             id='alert-circle'
-//             iconCategory={ICON_SPRITE_TYPES.ALERTS_AND_FEEDBACK}
-//             width={16}
-//             height={16}
-//             color={COLORS.ORANGE_SECONDARY}
-//           />
-//         );
-//       case 'loading':
-//         return (
-//           <SvgSpriteLoader
-//             id='alert-circle'
-//             iconCategory={ICON_SPRITE_TYPES.GENERAL}
-//             width={16}
-//             height={16}
-//             color={COLORS.GREEN_PRIMARY}
-//           />
-//         );
-//       default:
-//         return (
-//           <SvgSpriteLoader
-//             id='alert-circle'
-//             iconCategory={ICON_SPRITE_TYPES.ALERTS_AND_FEEDBACK}
-//             width={16}
-//             height={16}
-//             color={COLORS.GREEN_PRIMARY}
-//           />
-//         );
-//     }
-//   }, [type]);
-// };
 
 export const toast = {
   success: (message: string) => {
@@ -81,7 +11,7 @@ export const toast = {
     sonnerToast.warning(message);
   },
   loading: (message: string) => {
-    return sonnerToast.loading(message);
+    return sonnerToast.warning(message);
   },
   dismiss: (toastId: string | number) => {
     sonnerToast.dismiss(toastId);
