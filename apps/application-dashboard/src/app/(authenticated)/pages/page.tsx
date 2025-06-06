22db4f25ee313d53c61b9cb2dc153f17fbbd56be
chiragmittal-zamp <chirag@zamp.ai>
2025-06-06 17:25:52 +0530

    some more optimisations

--- apps/application-dashboard/src/app/(authenticated)/pages/page.tsx
+++ apps/application-dashboard/src/app/(authenticated)/pages/page.tsx
@@ -2,12 +2,10 @@
 
 import { useEffect } from 'react';
 import { useGetPagesQuery } from '@/apis/pages';
-import DynamicLottiePlayer from '@/components/DynamicLottiePlayer';
-import { ZAMP_LOGO_LOADER } from '@/constants/lottie/zamp-logo-loader';
 import { usePersistedPageNavigation } from '@/hooks/useLastVisitedPage';
 
 export default function Page() {
-  const { data: pages, isLoading } = useGetPagesQuery(undefined, {
+  const { data: pages } = useGetPagesQuery(undefined, {
     refetchOnMountOrArgChange: false,
   });
 
@@ -19,15 +17,5 @@ export default function Page() {
     }
   }, [pages]);
 
-  return (
-    <div className='flex h-full items-center justify-center'>
-      {isLoading || (pages || [])?.length > 0 ? (
-        <div className='z-50 flex h-[calc(100vh-200px)] w-full items-center justify-center bg-white'>
-          <DynamicLottiePlayer src={ZAMP_LOGO_LOADER} className='lottie-player h-[140px]' autoplay loop keepLastFrame />
-        </div>
-      ) : (
-        <p>No Pages Found</p>
-      )}
-    </div>
-  );
+  return null;
 }
--- apps/application-dashboard/src/app/login/page.tsx
+++ apps/application-dashboard/src/app/login/page.tsx
@@ -1,8 +1,14 @@
 'use client';
 
 import React from 'react';
+import { Provider } from 'react-redux';
 import { LoginForm } from 'modules/login/LoginForm';
+import { store } from '@/store';
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <Provider store={store}>
+      <LoginForm />
+    </Provider>
+  );
 }
