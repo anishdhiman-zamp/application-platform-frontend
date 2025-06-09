a976994c59e928869961c6a638ecc26af98daddc
Tushar Agarwal <tushar@zamp.ai>
2025-06-09 16:11:00 +0530

    cursor pointer fixes (#425)
    * tabs cursor pointer
    
    * cursor pointer in other components

--- packages/ui/src/components/ui/checkbox.tsx
+++ packages/ui/src/components/ui/checkbox.tsx
@@ -11,7 +11,7 @@ function Checkbox({ className, ...props }: React.ComponentProps<typeof CheckboxP
     <CheckboxPrimitive.Root
       data-slot='checkbox'
       className={cn(
-        'border-GRAY_400 hover:bg-GRAY_400 focus-visible:ring-ring data-[state=checked]:bg-GRAY_1000 data-[state=checked]:border-GRAY_600 peer h-3.5 w-3.5 shrink-0 rounded-[3px] border focus-visible:ring-1 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50',
+        'border-GRAY_400 hover:bg-GRAY_400 focus-visible:ring-ring data-[state=checked]:bg-GRAY_1000 data-[state=checked]:border-GRAY_600 peer h-3.5 w-3.5 shrink-0 cursor-pointer rounded-[3px] border focus-visible:ring-1 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50',
         className,
       )}
       {...props}
