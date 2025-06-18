5ed61ea9b7452ab5725e97403f40c911c4e227f4
sayancr777-maity <sayan@zamp.ai>
2025-06-18 18:57:14 +0530

    removed serviceprovider

--- apps/application-dashboard/src/app/_providers/providers.tsx
+++ apps/application-dashboard/src/app/_providers/providers.tsx
@@ -2,24 +2,24 @@
 
 import React, { Suspense } from 'react';
 import { Provider } from 'react-redux';
-import ServiceWorkerProvider from '@/app/_providers/ServiceWorkerProvider';
 import { AuthGuard } from '@/components/hoc/AuthGuard';
 import { RouteGuard } from '@/components/hoc/RouteGuard';
+import { useServiceWorker } from '@/hooks/useServiceWorker';
 import { FeatureFlagsProvider } from '@/modules/feature-flags/provider';
 import { store } from '@/store';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
+  useServiceWorker();
+
   return (
     <Suspense fallback={null}>
-      <ServiceWorkerProvider>
-        <Provider store={store}>
-          <AuthGuard loginRoute='/login'>
-            <FeatureFlagsProvider>
-              <RouteGuard>{children}</RouteGuard>
-            </FeatureFlagsProvider>
-          </AuthGuard>
-        </Provider>
-      </ServiceWorkerProvider>
+      <Provider store={store}>
+        <AuthGuard loginRoute='/login'>
+          <FeatureFlagsProvider>
+            <RouteGuard>{children}</RouteGuard>
+          </FeatureFlagsProvider>
+        </AuthGuard>
+      </Provider>
     </Suspense>
   );
 };
