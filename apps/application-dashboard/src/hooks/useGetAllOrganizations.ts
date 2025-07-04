4d8b0bb4d5956795be38f00da450daa8e92a0b8c
siddharth <siddharth@zamp.ai>
2025-07-04 11:57:44 +0530

    build fix

--- apps/application-dashboard/src/hooks/useGetAllOrganizations.ts
+++ apps/application-dashboard/src/hooks/useGetAllOrganizations.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { captureException } from '@sentry/nextjs';
 import { getApiDomainByRegion } from '@zamp-platform/api';
 import { useAppSelector } from 'hooks/toolkit';
 import { RootState } from 'store';
@@ -12,34 +13,49 @@ const useGetAllOrganizations = () => {
   const [error, setError] = useState<boolean>(false);
 
   const fetchOrganizations = async () => {
-    const domains = await getApiDomainByRegion(user?.user_email, false);
-    const organizations = await Promise.allSettled(
-      domains.map(async (result) => {
-        const response = await fetch(`${result.domain}/${API_ENDPOINTS.ORGANIZATIONS_GET}`, {
-          headers: { Accept: 'application/json' },
-          credentials: 'include',
-        });
-        const data = await response.json();
-        const organizationsWithRegion = Array.isArray(data)
-          ? data.map((org) => ({ ...org, region: result.region }))
-          : [];
+    setLoading(true);
+    setError(false);
+    try {
+      const domains = await getApiDomainByRegion(user?.user_email, false);
 
-        return { status: response.status, data: organizationsWithRegion };
-      }),
-    );
+      if (!domains || domains.length === 0) {
+        setOrganizations([]);
+        setLoading(false);
 
-    const successfulRegion = organizations.filter(
-      (result): result is PromiseFulfilledResult<{ status: number; data: Organization[] }> =>
-        result.status === 'fulfilled' && result.value.status === 200,
-    );
+        return;
+      }
+      const organizations = await Promise.allSettled(
+        domains?.map(async (result) => {
+          const response = await fetch(`${result.domain}/${API_ENDPOINTS.ORGANIZATIONS_GET}`, {
+            headers: { Accept: 'application/json' },
+            credentials: 'include',
+          });
+          const data = await response.json();
+          const organizationsWithRegion = Array.isArray(data)
+            ? data.map((org: Organization) => ({ ...org, region: result.region }))
+            : [];
 
-    setOrganizations(successfulRegion.flatMap((result) => result.value.data));
-    setLoading(false);
-    setError(organizations.every((result) => result.status === 'rejected'));
+          return { status: response.status, data: organizationsWithRegion };
+        }),
+      );
+
+      const successfulRegion = organizations.filter(
+        (result): result is PromiseFulfilledResult<{ status: number; data: Organization[] }> =>
+          result.status === 'fulfilled' && result.value.status === 200,
+      );
+
+      setOrganizations(successfulRegion.flatMap((result) => result.value.data));
+      setError(organizations.every((result) => result.status === 'rejected'));
+    } catch (error) {
+      setOrganizations([]);
+      setError(true);
+      captureException(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
-    setLoading(true);
     fetchOrganizations();
   }, [user?.user_email]);
 
