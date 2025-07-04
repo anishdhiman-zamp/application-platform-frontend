78449e6927793d508efe07866768c82b43324be4
siddharth <siddharth@zamp.ai>
2025-07-04 12:01:00 +0530

    prettier

--- apps/application-dashboard/src/modules/login/LoginForm.tsx
+++ apps/application-dashboard/src/modules/login/LoginForm.tsx
@@ -123,7 +123,6 @@ export const LoginForm = () => {
     try {
       const responses = await Promise.allSettled(
         domains?.map(async (domain) => {
-
           const apiUrl = `${domain.domain}/${API_ENDPOINTS.AUTH_INITIAL_LOGIN_FLOW_BY_EMAIL_POST}`;
 
           return fetch(apiUrl, {
