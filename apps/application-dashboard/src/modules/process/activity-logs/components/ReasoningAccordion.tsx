c5c92b2a86bb5c8beb0386e95b2c9f02835eae20
sayancr777-maity <sayan@zamp.ai>
2025-07-10 16:27:10 +0530

    new animation for accordion

--- apps/application-dashboard/src/modules/process/activity-logs/components/ReasoningAccordion.tsx
+++ apps/application-dashboard/src/modules/process/activity-logs/components/ReasoningAccordion.tsx
@@ -78,53 +78,60 @@ const ReasoningAccordion = ({ thoughtSteps, logGroupId, isLastLog, status }: Rea
     <Accordion
       type='single'
       collapsible
-      className='mt-2 w-full max-w-[485px] min-w-[180px]'
+      className='mt-2 mb-10 w-full max-w-[485px] min-w-[180px]'
       value={openValue}
       onValueChange={setOpenValue}
     >
       <RevealElement className='w-full'>
-        <AccordionItem value={ACCORDION_ITEM} className='border-GRAY_100 w-full rounded-t-md rounded-br-md border'>
-          {/* {isLastLog && status === LOG_STATUS.LOADING && (
-            <AccordionTrigger className='f-12-450 text-GRAY_900 w-full cursor-pointer gap-x-2 p-1.5'>
-              <motion.span
-                className='text-wrap break-words'
-                key={thoughtSteps?.at(-1)}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.3 }}
+        <AccordionItem value={ACCORDION_ITEM} className='w-full border-0'>
+          <div className='relative'>
+            {!(status === LOG_STATUS.LOADING && isLastLog) && (
+              <AccordionTrigger
+                className={cn(
+                  'f-12-450 text-GRAY_900 border-GRAY_100 absolute top-0 left-0 w-full gap-x-2 rounded-tl-md rounded-tr-md border p-1.5',
+                  openValue !== ACCORDION_ITEM && 'rounded-br-md',
+                )}
               >
-                {thoughtSteps?.length > 0 ? thoughtSteps?.at(-1) : 'See reasoning'}
-              </motion.span>
-            </AccordionTrigger>
-          )} */}
-          {status !== LOG_STATUS.LOADING && isLastLog && (
-            <AccordionTrigger className='f-12-450 text-GRAY_900 w-full cursor-pointer gap-x-2 p-1.5'>
-              <motion.span
-                className='text-wrap break-words'
-                key={thoughtSteps?.at(-1)}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.3 }}
-              >
-                {thoughtSteps?.length > 0 ? thoughtSteps?.at(-1) : 'See reasoning'}
-              </motion.span>
-            </AccordionTrigger>
-          )}
-          <AccordionContent
-            className={cn(
-              'f-12-450 flex max-h-40 w-full flex-col gap-y-2 overflow-y-scroll border-t p-4 [&::-webkit-scrollbar]:hidden',
-              isLastLog && status === LOG_STATUS.LOADING && 'border-transparent',
+                <motion.span
+                  className='text-wrap break-words'
+                  key={thoughtSteps?.at(-1)}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{
+                    duration: 0.3,
+                    delay: 0.1,
+                  }}
+                >
+                  {thoughtSteps?.at(-1) || 'See reasoning'}
+                </motion.span>
+              </AccordionTrigger>
             )}
-          >
-            {thoughtSteps?.map((step, index) => (
-              <ThoughtStepItem
-                key={index}
-                step={step}
-                isLast={index === thoughtSteps?.length - 1}
-                lastItemRef={lastItemRef}
-              />
-            ))}
-          </AccordionContent>
+
+            <motion.div
+              initial={{ opacity: 1 }}
+              animate={{ opacity: openValue !== ACCORDION_ITEM ? 0 : 1 }}
+              transition={{
+                duration: 0.3,
+                ease: 'easeOut',
+              }}
+            >
+              <AccordionContent
+                className={cn(
+                  'f-12-450 border-GRAY_100 flex max-h-40 w-full flex-col gap-y-2 overflow-y-scroll rounded-tl-md rounded-tr-md rounded-br-md border-x border-b px-4 pt-12 pb-4 [&::-webkit-scrollbar]:hidden',
+                  isLastLog && status === LOG_STATUS.LOADING && 'border-t pt-4',
+                )}
+              >
+                {thoughtSteps?.map((step, index) => (
+                  <ThoughtStepItem
+                    key={index}
+                    step={step}
+                    isLast={index === thoughtSteps?.length - 1}
+                    lastItemRef={lastItemRef}
+                  />
+                ))}
+              </AccordionContent>
+            </motion.div>
+          </div>
         </AccordionItem>
       </RevealElement>
     </Accordion>
