61a8904ce2e2422e7605beedf3668b6837863896
AdityaZamp <aditya.jain@zamp.ai>
2025-08-11 14:43:49 +0530

    increase the dataset row view cell's max width (#865)

--- apps/application-dashboard/src/modules/process/artifacts/components/pdf-dataset-artifact/dataset-row/DisplayField.tsx
+++ apps/application-dashboard/src/modules/process/artifacts/components/pdf-dataset-artifact/dataset-row/DisplayField.tsx
@@ -13,7 +13,7 @@ const DisplayField = ({ value, isCompleted, isClicked, onClick, onDoubleClick }:
   return (
     <div
       className={cn(
-        'f-12-500 bg-GRAY_100 text-GRAY_1000 max-h-40 w-fit max-w-[280px] cursor-pointer overflow-y-scroll rounded-md border border-transparent px-1.5 py-1 break-words transition-colors duration-200 select-none [scrollbar-width:none]',
+        'f-12-500 bg-GRAY_100 text-GRAY_1000 max-h-40 w-fit max-w-[560px] cursor-pointer overflow-y-scroll rounded-md border border-transparent px-1.5 py-1 break-words transition-colors duration-200 select-none [scrollbar-width:none]',
         {
           'bg-ORANGE_100 underline underline-offset-2': isCompleted,
           'border-BLUE_700': isClicked,
