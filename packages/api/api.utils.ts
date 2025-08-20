8304b24358bff68ea629efe8aec9dfd4e18c9731
chiragmittal-zamp <chirag@zamp.ai>
2025-08-20 16:41:43 +0530

    fix

--- packages/api/api.utils.ts
+++ packages/api/api.utils.ts
@@ -40,7 +40,7 @@ export const getApiDomainAndRegions = async (email = '') => {
 };
 
 const getApiDomain = (environment = '', region = '') => {
-  console.log('environment>>> ', environment);
+  console.log('environment test>>>', environment);
   switch (environment) {
     case 'production':
       return `https://api${region}.zamp.ai`;
