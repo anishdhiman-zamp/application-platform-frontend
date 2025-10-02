f5f8c712f0401a63ad7c8ca56ebcd65a6685d78a
AdityaZamp <aditya.jain@zamp.ai>
2025-10-02 16:11:23 +0530

    Revert "Dashboard downtime"

--- apps/application-dashboard/src/components/hoc/RouteGuard.tsx
+++ apps/application-dashboard/src/components/hoc/RouteGuard.tsx
@@ -11,8 +11,6 @@ import { useWindowDimensions } from 'hooks/useWindowDimensions';
 import ScreenSupport from 'modules/cards/ScreenSupport';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { checkScreenBreakpoint, getLeadingPathFromURL } from 'utils/common';
-import DashboardDowntime from '@/modules/cards/DashboardDowntime';
-import { LOCAL_STORAGE_KEYS } from '@/utils/localstorage';
 import { PAGE_SIZE } from 'components/common/table/table.constants';
 
 type AuthGuardPropsType = {
@@ -24,7 +22,6 @@ export const RouteGuard: FC<AuthGuardPropsType> = (props) => {
   const pathname = usePathname() || '';
   const searchParams = useSearchParams();
   const id = searchParams?.get('id');
-  const isGodMode = localStorage.getItem(LOCAL_STORAGE_KEYS.XZAMP_GOD_MODE);
 
   const currentPathName = getLeadingPathFromURL(pathname);
   const PAGES = getLeadingPathFromURL(ROUTES_PATH.PAGES);
@@ -79,10 +76,6 @@ export const RouteGuard: FC<AuthGuardPropsType> = (props) => {
   const breakpoint = checkScreenBreakpoint(width, height);
 
   if (breakpoint && ENVIRONMENT === ENVIRONMENT_TYPES.PRODUCTION) return <ScreenSupport />;
-
-  if (!isGodMode) {
-    return <DashboardDowntime />;
-  }
 
   return props.children;
 };
