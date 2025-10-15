f987faba8b4c1a2409fe298c3b3f9f5836997121
Aditya Jain <aditya.jain@zamp.ai>
2025-10-15 20:16:51 +0530

    loader

--- apps/application-dashboard/src/components/common/loader/ZampLogoGifLoader.tsx
+++ apps/application-dashboard/src/components/common/loader/ZampLogoGifLoader.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { cn } from '@zamp-platform/ui/utils';
 import Image from 'next/image';
-import { ZAMP_LOGO_LOADER_GIF } from '@/constants/icons';
+import { ZAMP_LOGO_LOADER_WEBP_CDN, ZAMP_LOGO_LOADER_WEBP_WITHOUT_CDN } from '@/constants/icons';
 
 interface ZampLogoGifLoaderProps {
   className?: string;
@@ -10,7 +10,16 @@ interface ZampLogoGifLoaderProps {
 const ZampLogoGifLoader: FC<ZampLogoGifLoaderProps> = ({ className }) => {
   return (
     <div className={cn('flex h-full w-full items-center justify-center rounded-tl-xl bg-white', className)}>
-      <Image src={ZAMP_LOGO_LOADER_GIF} alt='zamp logo loader' width={140} height={140} unoptimized />
+      <Image src={ZAMP_LOGO_LOADER_WEBP_CDN} alt='zamp logo loader' width={140} height={140} priority unoptimized />
+      <Image
+        src={ZAMP_LOGO_LOADER_WEBP_WITHOUT_CDN}
+        alt='zamp logo loader'
+        width={140}
+        height={140}
+        priority
+        unoptimized
+        className='hidden'
+      />
     </div>
   );
 };
--- apps/application-dashboard/src/constants/icons.ts
+++ apps/application-dashboard/src/constants/icons.ts
@@ -163,7 +163,9 @@ export const USERS_02 = '/icons/users-02.svg';
 export const FLEX_ALIGN_RIGHT = '/icons/flex-align-right.svg';
 
 //GIF
-export const ZAMP_LOGO_LOADER_GIF = '/gifs/zamp-logo-loader.gif';
+export const ZAMP_LOGO_LOADER_GIF = IMAGE_PREFIX + '/gifs/zamp-logo-loader.gif';
+export const ZAMP_LOGO_LOADER_WEBP_WITHOUT_CDN = IMAGE_PREFIX + '/gifs/zamp-logo-loader.webp';
+export const ZAMP_LOGO_LOADER_WEBP_CDN = IMAGE_PREFIX + '/gifs/zamp-logo-loader.webp';
 
 export const VERCEL_BLOB_BASE_URL = process.env.NEXT_PUBLIC_VERCEL_BLOB_BASE_URL;
 export const VERCEL_BLOB_ICON_URL = `${VERCEL_BLOB_BASE_URL}/icons`;
