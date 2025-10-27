5fd2631a153fee6b4c1abf36e196744894dd9a7a
Sayan <sayan@zamp.ai>
2025-10-27 19:21:07 +0530

    Column Resizing Capability for Tanstack Table (#1361)
    * feat/ column resizing
    
    * move to constants

---
+++ packages/tanstack-table/components/TanstackRow.tsx
@@ -0,0 +1,68 @@
+import { Row } from '@tanstack/react-table';
+import { Virtualizer } from '@tanstack/react-virtual';
+import { cn } from '@zamp-platform/ui/utils';
+import React from 'react';
+
+import { ActivityRunRowData } from '@/modules/process/process.types';
+
+import { isNonMovableColumn } from '../constants';
+import TanstackCell from './TanstackCell';
+
+interface TanstackRowProps {
+  row: Row<unknown>;
+  virtualRow: { index: number; start: number };
+  rowVirtualizer: Virtualizer<HTMLDivElement, Element>;
+  highlightedRowIndex?: number;
+  isHighlighted: boolean;
+  rowHighlighting: {
+    enabled?: boolean;
+  };
+  enhancedHandleRowClick: (rowData: ActivityRunRowData, rowIndex?: number) => void;
+  onRowClicked?: (rowData: ActivityRunRowData, rowIndex?: number) => void;
+  cellClass?: string;
+}
+
+const TanstackRow: React.FC<TanstackRowProps> = ({
+  row,
+  virtualRow,
+  rowVirtualizer,
+  isHighlighted,
+  rowHighlighting,
+  enhancedHandleRowClick,
+  onRowClicked,
+  cellClass,
+}) => (
+  <tr
+    data-index={virtualRow.index}
+    ref={(node) => rowVirtualizer?.measureElement?.(node)}
+    key={row?.id}
+    className={cn('group absolute flex cursor-pointer', isHighlighted && 'bg-BACKGROUND_GRAY_2')}
+    style={{
+      transform: `translateY(${virtualRow.start}px)`,
+      width: '100%',
+    }}
+    onClick={() =>
+      rowHighlighting?.enabled
+        ? enhancedHandleRowClick(row?.original as ActivityRunRowData, virtualRow.index)
+        : onRowClicked?.(row?.original as ActivityRunRowData, virtualRow.index)
+    }
+  >
+    {row.getVisibleCells().map((cell) => {
+      const ctx = { ...cell.getContext(), absoluteRowIndex: virtualRow.index };
+      const colId = cell.column.id;
+      const showCustomCell = isNonMovableColumn(colId);
+      return (
+        <TanstackCell
+          key={cell.id}
+          cell={cell}
+          ctx={ctx}
+          isHighlighted={isHighlighted}
+          cellClass={cellClass}
+          showCustomCell={showCustomCell}
+        />
+      );
+    })}
+  </tr>
+);
+
+export default TanstackRow;
