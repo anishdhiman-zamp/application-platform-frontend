14410b7e8ac4433758f196ec97cbae9bddc852b4
SidAtZamp <siddharth@zamp.ai>
2025-11-06 14:28:03 +0530

    Login error (#1416)
    * login page error
    
    * error fixed

--- apps/application-dashboard/src/modules/login/LoginForm.tsx
+++ apps/application-dashboard/src/modules/login/LoginForm.tsx
@@ -44,6 +44,7 @@ export const LoginForm = () => {
   const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e?.target?.value !== undefined) {
       setEmail(e.target.value);
+      setError(null);
     }
   };
 
@@ -141,7 +142,7 @@ export const LoginForm = () => {
       setHasError(false);
 
       if (response.status !== API_STATUS_CODES.OK) {
-        setError(respJson.error);
+        setError(respJson.error || respJson?.detail);
         removeFromLocalStorage(LOCAL_STORAGE_KEYS.LAST_LOGGED_IN_OIDC_EMAIL);
         setHasError(true);
         setLoading(false);
