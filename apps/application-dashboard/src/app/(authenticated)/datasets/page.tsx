b314b59cf737d54b036269140cb181bb756cfe0d
default <aditya.jain@zamp.ai>
2025-11-14 13:44:04 +0000

    listing page ssr

--- apps/application-dashboard/src/app/(authenticated)/datasets/page.tsx
+++ apps/application-dashboard/src/app/(authenticated)/datasets/page.tsx
@@ -1,16 +1,5 @@
-'use client';
-
-import { RowClickedEvent } from 'ag-grid-community';
-import { getDatasetRouteById } from 'constants/routeConfig';
 import Listing from 'modules/data';
-import { useRouter } from 'next/navigation';
 
 export default function DatasetsPage() {
-  const router = useRouter();
-
-  const onRowClicked = (event: RowClickedEvent) => {
-    router.push(getDatasetRouteById(event?.data?.id));
-  };
-
-  return <Listing onRowClicked={onRowClicked} />;
+  return <Listing />;
 }
--- apps/application-dashboard/src/modules/data/index.tsx
+++ apps/application-dashboard/src/modules/data/index.tsx
@@ -1,24 +1,33 @@
-import { FC, useMemo } from 'react';
+'use client';
+
+import { useMemo } from 'react';
 import { RowClickedEvent } from 'ag-grid-community';
 import { LISTING_COLUMNS } from 'modules/data/data.constants';
-import { ListingPropsType } from 'modules/data/data.types';
+import { useRouter } from 'next/navigation';
 import { useGetDatasetListingQuery } from '@/apis/dataset';
 import ZampLogoWebpLoader from '@/components/common/loader/ZampLogoWebpLoader';
 import CommonWrapper from '@/components/commonWrapper';
 import { SkeletonTypes } from '@/components/commonWrapper/commonWrapper.types';
+import { getDatasetRouteById } from '@/constants/routeConfig';
 import DataTable from 'components/common/table/DataTable';
 import { PAGE_SIZE } from 'components/common/table/table.constants';
 
-const Listing: FC<ListingPropsType> = ({ onRowClicked }) => {
-  const { data, isLoading } = useGetDatasetListingQuery({ page: 1, pageSize: PAGE_SIZE });
+const Listing = () => {
+  const { data, isLoading } = useGetDatasetListingQuery(
+    { page: 1, pageSize: PAGE_SIZE },
+    {
+      refetchOnMountOrArgChange: false,
+    },
+  );
   const columns = useMemo(() => LISTING_COLUMNS, []);
+  const router = useRouter();
 
   const handleRowClicked = (event: RowClickedEvent) => {
     const target = event?.event?.target as HTMLElement;
 
     if (target.closest('#edit-name-description')) return;
 
-    onRowClicked?.(event);
+    router.push(getDatasetRouteById(event?.data?.id));
   };
 
   return (
