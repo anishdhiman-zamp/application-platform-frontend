26dbc2b75c9449101e2c66bec8b514c56706d8dc
Aditya Jain <--global>
2025-12-01 13:47:18 +0530

    dummy commit

--- apps/application-dashboard/src/app/not-found.tsx
+++ apps/application-dashboard/src/app/not-found.tsx
@@ -1,7 +1,6 @@
 import { Button } from '@zamp-platform/ui';
 import Link from 'next/link';
 
-//not found page
 export default function NotFound() {
   return (
     <div className='flex min-h-screen flex-col items-center justify-center p-4'>
