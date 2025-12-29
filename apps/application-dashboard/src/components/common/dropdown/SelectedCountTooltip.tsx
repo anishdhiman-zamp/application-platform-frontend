2a3de085c88204ff5c4ed8f245f08637c2ba753a
default <aditya.jain@zamp.ai>
2025-12-29 10:31:06 +0000

    refactor SelectedCountTooltip component to use ALIGN_OPTIONS for alignment and cn utility for class names.

--- apps/application-dashboard/src/components/common/dropdown/SelectedCountTooltip.tsx
+++ apps/application-dashboard/src/components/common/dropdown/SelectedCountTooltip.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
+import { cn } from '@zamp-platform/ui/utils';
 import { SelectedCountTooltipPropsType } from 'types/common/components/dropdown/dropdown.types';
-import { SIDE_OPTIONS } from '@/types/commonTypes';
+import { ALIGN_OPTIONS, SIDE_OPTIONS } from '@/types/commonTypes';
 import TooltipV2 from 'components/common/TooltipV2';
 
 const SelectedCountTooltip: FC<SelectedCountTooltipPropsType> = ({ value, tooltipBodyClassName }) => {
@@ -8,7 +9,7 @@ const SelectedCountTooltip: FC<SelectedCountTooltipPropsType> = ({ value, toolti
     <div>
       <TooltipV2
         side={SIDE_OPTIONS.BOTTOM}
-        align='end'
+        align={ALIGN_OPTIONS.END}
         alignOffset={-34}
         disabled={value?.length === 0}
         tooltipBody={
@@ -20,7 +21,7 @@ const SelectedCountTooltip: FC<SelectedCountTooltipPropsType> = ({ value, toolti
             ))}
           </div>
         }
-        tooltipClassName={`bg-GRAY_700 text-white f-12-300 px-3! py-2! ${tooltipBodyClassName}`}
+        tooltipClassName={cn('bg-GRAY_700 text-white f-12-300 px-3! py-2! ', tooltipBodyClassName)}
       >
         {!!value?.length && (
           <div className='bg-BASE_PRIMARY f-10-600 flex h-4 w-5 items-center justify-center'>{value?.length}</div>
