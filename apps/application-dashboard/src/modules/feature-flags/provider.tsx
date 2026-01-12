2bd73e9291236adb5ec17ad451eadb4b7245fc0c
Aditya Jain <--global>
2026-01-12 16:51:45 +0530

    fix enviorment

--- apps/application-dashboard/src/modules/feature-flags/provider.tsx
+++ apps/application-dashboard/src/modules/feature-flags/provider.tsx
@@ -3,6 +3,7 @@
 import { ReactNode, useMemo } from 'react';
 import { LAUNCH_DARKLY_CLIENT_SIDE_ID } from 'constants/featureFlags';
 import { LDProvider } from 'launchdarkly-react-client-sdk';
+import { ENVIRONMENT } from '@/constants/common.constants';
 import { getUserSession } from '@/utils/cookie';
 
 type Props = {
@@ -22,9 +23,9 @@ export const FeatureFlagsProvider = ({ children }: Props) => {
     [userSession?.user_id, userSession?.user_email, userSession?.default_org_id],
   );
 
-  // if (ENVIRONMENT === 'local') {
-  //   return children;
-  // }
+  if (ENVIRONMENT === 'local') {
+    return children;
+  }
 
   return (
     <LDProvider clientSideID={LAUNCH_DARKLY_CLIENT_SIDE_ID} context={context}>
