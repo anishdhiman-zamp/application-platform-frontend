3b737bdf732d5f1a83c5073407ab07e54608be58
default <aditya.jain@zamp.ai>
2026-01-22 09:17:26 +0000

    fix the file name

--- apps/application-dashboard/src/modules/chatbot/NewPaceAvatar.tsx
+++ apps/application-dashboard/src/modules/chatbot/NewPaceAvatar.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const NewPaceAvatar = () => {
   return (
     <div className='grid h-5 min-h-5 w-5 min-w-5 place-items-center rounded-full'>
-      <Image src='/icons/pace/pace-new.svg' alt='Pace Avatar' height={20} width={20} />
+      <Image src='/icons/pace/pace-avatar.svg' alt='Pace Avatar' height={20} width={20} />
     </div>
   );
 };
