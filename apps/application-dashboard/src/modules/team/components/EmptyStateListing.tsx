197fd6ae14dd2d0674491c5c0cce5d77541f6fa4
default <aditya.jain@zamp.ai>
2026-01-26 12:22:14 +0000

    Enhance chat history functionality with search feature and debounce logic

--- apps/application-dashboard/src/modules/team/components/EmptyStateListing.tsx
+++ apps/application-dashboard/src/modules/team/components/EmptyStateListing.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react';
+import { cn } from '@zamp-platform/ui/utils';
 import { EmptyStateListingPropsType } from 'modules/team/people.types';
 import ImageKitImage from '@/components/ImageKitImage';
 import { TEAM_MEMBERS_EMPTY_STATE } from '@/constants/icons';
 
-const EmptyStateListing: FC<EmptyStateListingPropsType> = ({ title = 'Nothing to show up' }) => {
+const EmptyStateListing: FC<EmptyStateListingPropsType> = ({ title = 'Nothing to show up', className }) => {
   return (
-    <div className='flex h-3/5 w-full items-center justify-center'>
+    <div className={cn('flex h-3/5 w-full items-center justify-center', className)}>
       <div className='flex flex-col items-center justify-center gap-y-3'>
         <ImageKitImage src={TEAM_MEMBERS_EMPTY_STATE} alt='No team members were added' width={222} height={181} />
         <span className='f-16-500 text-GRAY_600 flex h-3/5 w-full items-center justify-center'>{title}</span>
