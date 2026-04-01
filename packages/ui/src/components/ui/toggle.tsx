d690b358601cc1da10637910e9212deef7969894
default <saurabh@zamp.ai>
2026-04-01 18:48:32 +0000

    remove outline

--- packages/ui/src/components/ui/toggle.tsx
+++ packages/ui/src/components/ui/toggle.tsx
@@ -6,7 +6,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { ComponentPropsWithRef } from 'react';
 
 const toggleVariants = cva(
-  'inline-flex items-center justify-center gap-1 rounded-full text-sm font-medium transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0',
+  'inline-flex items-center justify-center gap-1 rounded-full text-sm font-medium transition-colors focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0',
   {
     variants: {
       variant: {
